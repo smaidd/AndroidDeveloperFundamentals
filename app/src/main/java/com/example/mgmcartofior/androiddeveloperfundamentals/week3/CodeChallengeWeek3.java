111efe425d40e156dc02712b7cd6704ae19e2367
smaidd <ionita.alex16@gmail.com>
2018-12-02 12:37:40 +0200

    week3.3

---
+++ app/src/main/java/com/example/mgmcartofior/androiddeveloperfundamentals/week3/CodeChallengeWeek3.java
@@ -0,0 +1,15 @@
+package com.example.mgmcartofior.androiddeveloperfundamentals.week3;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.alex.androidfundamentals.R;
+
+public class CodeChallengeWeek3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.codechallenge_2_week3);
+    }
+}
